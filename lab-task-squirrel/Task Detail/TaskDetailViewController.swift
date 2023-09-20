14c17cd5651ae7b55962ef18738705779add08b4
sofia <sofitorres929@gmail.com>
2023-09-20 17:43:45 -0400

    Created, setup and presented the image picker

--- lab-task-squirrel/Task Detail/TaskDetailViewController.swift
+++ lab-task-squirrel/Task Detail/TaskDetailViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import MapKit
+import PhotosUI
+
 
 // TODO: Import PhotosUI
 
@@ -59,11 +61,54 @@ class TaskDetailViewController: UIViewController {
 
     @IBAction func didTapAttachPhotoButton(_ sender: Any) {
         // TODO: Check and/or request photo library access authorization.
-
+        // If authorized, show photo picker, otherwise request authorization.
+        // If authorization denied, show alert with option to go to settings to update authorization.
+        if PHPhotoLibrary.authorizationStatus(for: .readWrite) != .authorized {
+            // Request photo library access
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { [weak self] status in
+                switch status {
+                case .authorized:
+                    // The user authorized access to their photo library
+                    // show picker (on main thread)
+                    DispatchQueue.main.async {
+                        self?.presentImagePicker()
+                    }
+                default:
+                    // show settings alert (on main thread)
+                    DispatchQueue.main.async {
+                        // Helper method to show settings alert
+                        self?.presentGoToSettingsAlert()
+                    }
+                }
+            }
+        } else {
+            // Show photo picker
+            presentImagePicker()
+        }
     }
 
     private func presentImagePicker() {
         // TODO: Create, configure and present image picker.
+        // Create a configuration object
+        var config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+
+        // Set the filter to only show images as options (i.e. no videos, etc.).
+        config.filter = .images
+
+        // Request the original file format. Fastest method as it avoids transcoding.
+        config.preferredAssetRepresentationMode = .current
+
+        // Only allow 1 image to be selected at a time.
+        config.selectionLimit = 1
+
+        // Instantiate a picker, passing in the configuration.
+        let picker = PHPickerViewController(configuration: config)
+
+        // Set the picker delegate so we can receive whatever image the user picks.
+        picker.delegate = self
+
+        // Present the picker.
+        present(picker, animated: true)
 
     }
 
@@ -115,4 +160,12 @@ extension TaskDetailViewController {
 
         present(alertController, animated: true)
     }
+}
+
+extension TaskDetailViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+    
+    }
+    
+
 }
